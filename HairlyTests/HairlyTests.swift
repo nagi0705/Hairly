3afbd8b65b6f98689bd245d77c2ed2debe9642fc
nagi0705 <bellreuben1126@gmail.com>
2025-02-07 20:35:24 +0900

    1. プロジェクトの準備

---
+++ HairlyTests/HairlyTests.swift
@@ -0,0 +1,17 @@
+//
+//  HairlyTests.swift
+//  HairlyTests
+//
+//  Created by 小松崎凪沙 on 2025/02/07.
+//
+
+import Testing
+@testable import Hairly
+
+struct HairlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
